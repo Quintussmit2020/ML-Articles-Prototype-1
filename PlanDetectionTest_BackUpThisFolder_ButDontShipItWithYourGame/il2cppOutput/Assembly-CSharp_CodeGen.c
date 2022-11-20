112b5b2de2baeea9e30f8b911cf308094aa9c24f
Quintussmit2020 <quintus.smit@gmail.com>
2022-11-20 09:25:02 +0000

    Initial commit

---
+++ PlanDetectionTest_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,83 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void PlaneExample_Awake_m9383CFA520BB1271E451F7088D948523AC4A5D81 (void);
+extern void PlaneExample_OnDestroy_m75A6BB0D7E01DF98A687959CE93F48CE7755828F (void);
+extern void PlaneExample_Start_m344BA133DC83E80EF0386FAF681AFD83BD5ABCE4 (void);
+extern void PlaneExample_Trigger_performed_m3C33002C441EE0239F6D9E44D005946F19A08CFF (void);
+extern void PlaneExample_Update_m63BD62EA40CDE7B1C9CCF32E6C248A812C0F7824 (void);
+extern void PlaneExample_OnPermissionGranted_m301105C2D3734E3D05F0CB7BCDAED2F95F1FDA3B (void);
+extern void PlaneExample_OnPermissionDenied_m6843BAD03982FB9B40A39919711CD203D9773217 (void);
+extern void PlaneExample__ctor_mF886FCA24554BB58159B838FEBD4ABF26737293D (void);
+extern void PlanePrefabExample_Start_m48FB493FE6F7B69C71F94AC51E833C7EC2CDF4A2 (void);
+extern void PlanePrefabExample_ColorClassify_m85589A5028FFB33A7063174C72C53F720880146C (void);
+extern void PlanePrefabExample__ctor_m1EBC280DC21CD4B3F502913F2F6622FA26BC09DC (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_IsEditorOnly_m59E10A16B0BDEA496377805E39C3D90B1E2C4815 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_Get_mDAD527916A4779DB4ACAFEADC3BA7372671FA647 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes__ctor_mE2C680837D47039E3F06032AA8C63FEDB7E783A5 (void);
+static Il2CppMethodPointer s_methodPointers[14] = 
+{
+	PlaneExample_Awake_m9383CFA520BB1271E451F7088D948523AC4A5D81,
+	PlaneExample_OnDestroy_m75A6BB0D7E01DF98A687959CE93F48CE7755828F,
+	PlaneExample_Start_m344BA133DC83E80EF0386FAF681AFD83BD5ABCE4,
+	PlaneExample_Trigger_performed_m3C33002C441EE0239F6D9E44D005946F19A08CFF,
+	PlaneExample_Update_m63BD62EA40CDE7B1C9CCF32E6C248A812C0F7824,
+	PlaneExample_OnPermissionGranted_m301105C2D3734E3D05F0CB7BCDAED2F95F1FDA3B,
+	PlaneExample_OnPermissionDenied_m6843BAD03982FB9B40A39919711CD203D9773217,
+	PlaneExample__ctor_mF886FCA24554BB58159B838FEBD4ABF26737293D,
+	PlanePrefabExample_Start_m48FB493FE6F7B69C71F94AC51E833C7EC2CDF4A2,
+	PlanePrefabExample_ColorClassify_m85589A5028FFB33A7063174C72C53F720880146C,
+	PlanePrefabExample__ctor_m1EBC280DC21CD4B3F502913F2F6622FA26BC09DC,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_IsEditorOnly_m59E10A16B0BDEA496377805E39C3D90B1E2C4815,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_Get_mDAD527916A4779DB4ACAFEADC3BA7372671FA647,
+	UnitySourceGeneratedAssemblyMonoScriptTypes__ctor_mE2C680837D47039E3F06032AA8C63FEDB7E783A5,
+};
+static const int32_t s_InvokerIndices[14] = 
+{
+	9338,
+	9338,
+	9338,
+	7485,
+	9338,
+	7237,
+	7237,
+	9338,
+	9338,
+	9338,
+	9338,
+	16238,
+	16280,
+	9338,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	14,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+# ML-Articles-Prototype-1
+ Prototype project for the first series of articles on the ML 2 features
